06c56e6ef7a059fa238dc81c63f09b5e077f7d62
Agnieszka-18 <135909505+Agnieszka-18@users.noreply.github.com>
2023-06-08 04:02:36 +0200

    Update videoPlayer.js

--- videoPlayer.js
+++ videoPlayer.js
@@ -20,3 +20,18 @@ window.VideoPlayer = {
   }
 };
 
+
+function embedYouTubeVideo(containerId, videoUrl) {
+  var videoId = videoUrl.split('v=')[1].split('&')[0];
+
+  var iframe = document.createElement('iframe');
+  iframe.width = '560';
+  iframe.height = '315';
+  iframe.src = 'https://www.youtube.com/embed/' + videoId;
+  iframe.allowFullscreen = true;
+
+  var container = document.getElementById(containerId);
+  container.appendChild(iframe);
+}
+
+
